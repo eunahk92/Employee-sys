a82ec780012dc23b2037cc8a285ddc01754d5d72
Eunah Kim <Eunahkim92@gmail.com>
2020-06-17 14:30:08 -0400

    Set up connection to database

--- server.js
+++ server.js
@@ -3,4 +3,20 @@ const mysql = require("mysql");
 const dotenv = require("dotenv");
 
 dotenv.config();
+const { DB_HOST, PORT, DB_USER, DB_PASSWORD } = process.env;
 
+const connection = mysql.createConnection({
+    host: DB_HOST,
+    port: PORT,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: "ex_company_db"
+});
+  
+connection.connect(err => {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
