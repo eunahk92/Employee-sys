0f134fcc5f6f0a62683557cf70c4846cf56b7b07
Eunah Kim <Eunahkim92@gmail.com>
2020-06-17 18:11:06 -0400

    Modularize main menu prompt

---
+++ public/mainMenu.js
@@ -0,0 +1,13 @@
+module.exports = {
+    type: "list",
+    name: "action",
+    message: "What would you like to do?",
+    choices: [
+        "Add an employee",
+        "View employees",
+        "Add a role",
+        "View roles",
+        "Add a department",
+        "View departments"
+    ]
+}
