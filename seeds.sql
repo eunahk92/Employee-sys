c982c78498a1e638157a96cd6a4ba7be422e5ea1
Eunah Kim <Eunahkim92@gmail.com>
2020-06-17 23:28:15 -0400

    Seed the database

---
+++ seeds.sql
@@ -0,0 +1,28 @@
+USE ex_company_db;
+
+INSERT INTO departments (dept_name)
+VALUES ("IT");
+
+INSERT INTO departments (dept_name)
+VALUES ("HR");
+
+INSERT INTO departments (dept_name)
+VALUES ("Marketing");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Software Engineer", 100000, 1);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Payroll Specialist", 70000, 2);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("PR", 70000, 3);
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES ("Chester", "Kim", 2);
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES ("Yoshi", "Nguyen", 1);
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES ("Harry", "Potter", 3);
