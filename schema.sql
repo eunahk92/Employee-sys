e5315029a715db8c6bf39fd3808627ab8f85bfa1
Eunah Kim <Eunahkim92@gmail.com>
2020-06-17 23:28:01 -0400

    Change table names and data types

--- schema.sql
+++ schema.sql
@@ -4,25 +4,25 @@ CREATE DATABASE ex_company_db;
 
 USE ex_company_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
   id INT AUTO_INCREMENT,
   dept_name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
   id INT AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL(19,9) NOT NULL,
+  salary INT(15) NOT NULL,
   department_id INT(10),
   PRIMARY KEY (id)
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
   id INT AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT(10),
-  manager_id INT(10),
+  role_id INT(5),
+  manager_id INT(5),
   PRIMARY KEY (id)
 );
