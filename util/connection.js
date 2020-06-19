9526873fa365e0201ad3120cfb6fe0b2fbec07f5
Eunah Kim <Eunahkim92@gmail.com>
2020-06-18 23:58:51 -0400

    Modularize mySQL connection

---
+++ util/connection.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const { DB_HOST, PORT, DB_USER, DB_PASSWORD } = process.env;
+
+const connection = mysql.createConnection({
+    host: DB_HOST,
+    port: PORT,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: "ex_company_db"
+});
+  
+connection.connect(err => {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+});
+
+module.exports = connection;
