3dd9dc942fc02587d818f9522af87dbe962d0652
Eunah Kim <Eunahkim92@gmail.com>
2020-06-18 23:57:27 -0400

    Modularize inquirer prompts

---
+++ util/prompts.js
@@ -0,0 +1,60 @@
+const inquirer = require("inquirer");
+
+
+module.exports = {
+    addEmployee(rolesTitle, employeeNames) {
+        return inquirer.prompt([
+            {
+                type: "input",
+                name: "first_name",
+                message: "What is the employee's first name?"
+            },
+            {
+                type: "input",
+                name: "last_name",
+                message: "What is the employee's last name?"
+            },
+            {
+                type: "list",
+                name: "role",
+                message: "What is the employee's role?",
+                choices: rolesTitle
+            },
+            {
+                type: "list",
+                name: "manager",
+                message: "Who is the employee's manager?",
+                choices: employeeNames
+            }
+        ])
+    },
+    addRole(departmentNames) {
+        return inquirer.prompt([
+            {
+                type: "input",
+                name: "title",
+                message: "What is the role?"
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: "How much is the salary for the role?"
+            },
+            {
+                type: "list",
+                name: "dept",
+                message: "What department is that role in?",
+                choices: departmentNames
+            }
+        ])
+    },
+    addDepartment() {
+        return inquirer.prompt([
+            {
+                type: "input",
+                name: "dept_name",
+                message: "What department would you like to add?"
+            }
+        ])
+    }
+}
