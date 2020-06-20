2ee4969003ba897c3c93bf2f90cfa690a71c97c7
Eunah Kim <Eunahkim92@gmail.com>
2020-06-20 12:47:13 -0400

    Add more data to seed database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,28 +1,33 @@
 USE ex_company_db;
 
 INSERT INTO departments (dept_name)
-VALUES ("IT");
-
-INSERT INTO departments (dept_name)
-VALUES ("HR");
-
-INSERT INTO departments (dept_name)
-VALUES ("Marketing");
+VALUES 
+    ("Sales"),
+    ("Human Resources"),
+    ("Marketing"),
+    ("IT");
 
 INSERT INTO roles (title, salary, dept_id)
-VALUES ("Software Engineer", 90000, 1);
-
-INSERT INTO roles (title, salary, dept_id)
-VALUES ("Admin", 50000, 2);
-
-INSERT INTO roles (title, salary, dept_id)
-VALUES ("PR", 60000, 3);
+VALUES 
+	("Event Sales", 80500, 1),
+	("Event Coordinator", 66000, 1),
+	("Admin", 60000, 2),
+	("Payroll Specialist", 71000, 2),
+	("Public Relations", 86000, 3),
+	("Graphic Designer", 87500, 3),
+	("Software Developer", 115000, 4),
+	("Full Stack Developer", 105000, 4);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("Chester", "Kim", 2, 1);
-
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("Yoshi", "Nguyen", 1, 3);
-
+VALUES 
+	("Chester", "Kim", 1, 5),
+	("Yoshi", "Nguyen", 2, 6),
+	("Patrick", "Star", 3, 7),
+	("Harry", "Potter", 4, 8);
+    
 INSERT INTO employees (first_name, last_name, role_id)
-VALUES ("Harry", "Potter", 3);
+VALUES
+	("Peter", "Pan", 1),
+	("Spongebob", "Squarepants", 2),
+	("Heromine", "Granger", 3),
+	("Cardi", "B", 4);
